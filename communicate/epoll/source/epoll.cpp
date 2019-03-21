10c4e576e1aff2fde61a30e8580616a4a00b63e9
ezxinzh <1205331988@qq.com>
2019-03-21 14:01:35 +0800

    [task]create new project
    Description:
        1.N/A

---
+++ communicate/epoll/source/epoll.cpp
@@ -0,0 +1,19 @@
+/*
+ * epoll.cpp
+ *
+ *  Created on: Mar 21, 2019
+ *      Author: root
+ */
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    printf("welcome to my world.\n");
+    return 0;
+}
+
+
