818c5537437dcda1b6bff6566a9cd4586ee45cbe
ezxinzh <1205331988@qq.com>
2019-03-21 22:10:33 +0800

    [task] add tcp server function by using epoll.

--- communicate/epoll/include/epoll.h
+++ communicate/epoll/include/epoll.h
@@ -1,10 +1,3 @@
-/*
- * epoll.h
- *
- *  Created on: Mar 21, 2019
- *      Author: root
- */
-
 #ifndef COMMUNICATE_EPOLL_INCLUDE_EPOLL_H_
 #define COMMUNICATE_EPOLL_INCLUDE_EPOLL_H_
 
--- communicate/epoll/source/epoll.cpp
+++ communicate/epoll/source/epoll.cpp
@@ -17,36 +17,42 @@
 #include <sys/epoll.h>
 #include <unistd.h>
 #include <sys/types.h>
-#define IPADDRESS "127. 0. 0. 1"
-#define PORT 6666
+#include "../include/epoll.h"
+
+#define IPADDRESS "172.18.76.147"
+#define PORT 32006
 #define MAXSIZE 1024
 #define LISTENQ 5
 #define FDSIZE 1000
 #define EPOLLEVENTS 100
 
-int socket_bind( const char* ip, int port)
+int socket_bind(const char* ip, int port)
 {
     int listenfd;
     struct sockaddr_in servaddr;
     listenfd = socket( AF_INET, SOCK_STREAM, 0);
     if (listenfd == -1)
     {
-        perror(" socket error:");
+        perror("socket error:");
         exit( 1);
     }
     bzero(& servaddr, sizeof( servaddr));
-    servaddr. sin_family = AF_INET;
+    servaddr.sin_family = AF_INET;
     inet_pton( AF_INET, ip,& servaddr. sin_addr);
-    servaddr. sin_port = htons( port);
+    servaddr.sin_port = htons(port);
     if (bind( listenfd,( struct sockaddr*)& servaddr, sizeof( servaddr)) == -1)
     {
         perror(" bind error: ");
         exit( 1);
     }
+    else
+    {
+        printf("======bind addr:%s port:%d successfully.\n", inet_ntoa(servaddr.sin_addr), port);
+    }
     return listenfd;
 }
 
-void do_epoll( int listenfd)
+void do_epoll(int listenfd)
 {
     int epollfd;
     struct epoll_event events[ EPOLLEVENTS];
@@ -57,7 +63,7 @@ void do_epoll( int listenfd)
     epollfd = epoll_create( FDSIZE);
     /*添加监听描述符事件*/
     add_event( epollfd, listenfd, EPOLLIN);
-    while( 1)
+    while(1)
     {
         /*获取已经准备好的描述符事件*/
         ret = epoll_wait( epollfd, events, EPOLLEVENTS,- 1);
@@ -66,14 +72,14 @@ void do_epoll( int listenfd)
     close( epollfd);
 }
 
-void handle_events( int epollfd, struct epoll_event *events, int num, int listenfd, char *buf)
+void handle_events(int epollfd, struct epoll_event *events, int num, int listenfd, char *buf)
 {
     int i;
     int fd;
     /*进行选好遍历*/
     for (i = 0; i < num; i++)
     {
-        fd = events[ i].data.fd;
+        fd = events[i].data.fd;
         /*根据描述符的类型和事件类型进行处理*/
         if ((fd == listenfd) &&(events[ i]. events & EPOLLIN))
             handle_accpet( epollfd, listenfd);
@@ -84,58 +90,58 @@ void handle_events( int epollfd, struct epoll_event *events, int num, int listen
     }
 }
 
-void handle_accpet( int epollfd, int listenfd)
+void handle_accpet(int epollfd, int listenfd)
 {
     int clifd;
     struct sockaddr_in cliaddr;
     socklen_t cliaddrlen;
     clifd = accept( listenfd,( struct sockaddr*)& cliaddr,& cliaddrlen);
     if (clifd == -1)
-        perror(" accpet error:");
+        perror("accpet error:");
     else
     {
-        printf(" accept a new client: %s:% d\ n", inet_ntoa( cliaddr.sin_addr), cliaddr.sin_port);
+        printf("======accept a new client: addr:%s port:%d\n", inet_ntoa( cliaddr.sin_addr), cliaddr.sin_port);
         /*添加一个客户描述符和事件*/
         add_event( epollfd, clifd, EPOLLIN);
     }
 }
 
-void do_read( int epollfd, int fd, char *buf)
+void do_read(int epollfd, int fd, char *buf)
 {
     int nread;
-    nread = read( fd, buf, MAXSIZE);
+    nread = read(fd, buf, MAXSIZE);
     if (nread == -1)
     {
-        perror(" read error:");
-        close( fd);
+        perror("======read error:\n");
+        close(fd);
         delete_event( epollfd, fd, EPOLLIN);
     }
     else if (nread == 0)
     {
-        fprintf( stderr," client close.\ n");
-        close( fd);
+        fprintf( stderr,"======client close.\n");
+        close(fd);
         delete_event( epollfd, fd, EPOLLIN);
     }
     else
     {
-        printf(" read message is : %s", buf);
+        printf("======read message is : %s\n", buf);
         /*修改描述符对应的事件，由读改为写*/
         modify_event( epollfd, fd, EPOLLOUT);
     }
 }
 
-void do_write( int epollfd, int fd, char *buf)
+void do_write(int epollfd, int fd, char *buf)
 {
     int nwrite;
     nwrite = write( fd, buf, strlen( buf));
     if (nwrite == -1)
     {
         perror(" write error:");
-        close( fd);
-        delete_event( epollfd, fd, EPOLLOUT);
+        close(fd);
+        delete_event(epollfd, fd, EPOLLOUT);
     }
     else
-        modify_event( epollfd, fd, EPOLLIN);
+        modify_event(epollfd, fd, EPOLLIN);
     memset( buf, 0, MAXSIZE);
 }
 
@@ -166,7 +172,7 @@ void modify_event( int epollfd, int fd, int state)
 int main( int argc, char *argv[])
 {
     int listenfd;
-    listenfd = socket_bind( IPADDRESS, PORT);
+    listenfd = socket_bind(IPADDRESS, PORT);
     listen( listenfd, LISTENQ);
     do_epoll( listenfd);
     return 0;
